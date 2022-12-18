78f6ea7b49c2aba673b059d56f35b7f063c23eec
pthota27 <pthota27@gmail.com>
2022-12-18 15:37:22 -0600

    Add files via upload

---
+++ hellopython.py
@@ -0,0 +1,22 @@
+x=2
+print x
+mylist=[0,1,2]
+print mylist[0]
+lotsofhellos = "hello" * 10
+print lotsofhellos
+print [1,2,3]*10
+print mylist*10
+name ="John Doe"
+bal=63.44
+print "hello %s you balance is $ %.2f " %(name, bal)
+print len(name)
+print name.index("h")
+print name.count(" ")
+print name.upper()
+
+def sleep_in(weekday, vacation):
+  if weekday and vacation:
+     return True
+  Else:
+     return false
+	 
